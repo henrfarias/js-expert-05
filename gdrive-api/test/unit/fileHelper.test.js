5ea313d22eede4fd1c40b0e6c061d48e91da32f3
henrfarias <henrique.biasibetti@gmail.com>
2021-09-07 01:23:13 -0300

    test: tests with mocks

---
+++ gdrive-api/test/unit/fileHelper.test.js
@@ -0,0 +1,57 @@
+import { describe, test, expect, jest } from '@jest/globals';
+import fs from 'fs/promises';
+import Routes from '../../src/routes.js';
+import FileHelper from '../../src/fileHelper.js';
+
+describe('#FileHelper', () => {
+  describe('#getFilesStatus', () => {
+    test('it should return files statuses in correct format', async () => {
+      const statMock = {
+        dev: 64768,
+        mode: 33204,
+        nlink: 1,
+        uid: 1000,
+        gid: 1000,
+        rdev: 0,
+        blksize: 4096,
+        ino: 28445984,
+        size: 618565,
+        blocks: 1216,
+        atimeMs: 1630984554866.289,
+        mtimeMs: 1630984554614.2878,
+        ctimeMs: 1630984554618.2878,
+        birthtimeMs: 1630984554614.2878,
+        atime: '2021-09-07T03:15:54.866Z',
+        mtime: '2021-09-07T03:15:54.614Z',
+        ctime: '2021-09-07T03:15:54.618Z',
+        birthtime: '2021-09-07T03:15:54.614Z',
+      };
+
+      const mockUser = 'henrfarias';
+      process.env.USER = mockUser;
+      const filename = 'file.png';
+
+      jest
+        .spyOn(fs, fs.readdir.name)
+        .mockResolvedValue([filename]);
+
+      jest
+        .spyOn(fs, fs.stat.name)
+        .mockResolvedValue(statMock);
+
+      const result = await FileHelper.getFilesStatus('/tmp');
+
+      const expectedResult = [
+        {
+          size: '619 kB',
+          lastModified: statMock.birthtime,
+          owner: mockUser,
+          file: filename,
+        },
+      ];
+
+      expect(fs.stat).toHaveBeenCalledWith(`/tmp/${filename}`);
+      expect(result).toMatchObject(expectedResult);
+    });
+  });
+});
--- gdrive-api/test/unit/routes.test.js
+++ gdrive-api/test/unit/routes.test.js
@@ -2,6 +2,21 @@ import { describe, test, expect, jest } from '@jest/globals';
 import Routes from '../../src/routes.js';
 
 describe('#Routes test suite', () => {
+  const defaultParams = {
+    request: {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      method: '',
+      body: {},
+    },
+    response: {
+      setHeader: jest.fn(),
+      writeHead: jest.fn(),
+      end: jest.fn(),
+    },
+    values: () => Object.values(defaultParams),
+  };
   describe('#setSocketInstance', () => {
     test('setSocket should store io instance', () => {
       const routes = new Routes();
@@ -16,21 +31,6 @@ describe('#Routes test suite', () => {
   });
 
   describe('#handler', () => {
-    const defaultParams = {
-      request: {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        method: '',
-        body: {},
-      },
-      response: {
-        setHeader: jest.fn(),
-        writeHead: jest.fn(),
-        end: jest.fn(),
-      },
-      values: () => Object.values(defaultParams),
-    };
     test('given an inexistent route it should choose default route', async () => {
       const routes = new Routes();
       const params = {
@@ -92,7 +92,28 @@ describe('#Routes test suite', () => {
   });
 
   describe('#get', () => {
-    test.todo('given method GET it should list all files downloaded');
+    test('given method GET it should list all files downloaded', async () => {
+      const routes = new Routes();
+      const params = {
+        ...defaultParams,
+      };
+      const filesStatusesMock = [
+        {
+          size: '619 kB',
+          lastModified: '2021-09-07T03:15:54.614Z',
+          owner: 'henrfarias',
+          file: 'file.png',
+        },
+      ];
+      jest
+        .spyOn(routes.fileHelper, routes.fileHelper.getFilesStatus.name)
+        .mockResolvedValue(filesStatusesMock);
+
+      params.request.method = 'GET';
+      await routes.handler(...params.values());
     
+      expect(params.response.writeHead).toHaveBeenCalledWith(200);
+      expect(params.response.end).toHaveBeenCalledWith(JSON.stringify(filesStatusesMock));
+    });
   });
 });
