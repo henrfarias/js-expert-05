2f6c0ca4b03766a4f051f0d90b60f9ffb3c81cad
henrfarias <henrique.biasibetti@gmail.com>
2021-09-10 13:42:15 -0300

    feat: implement uploadHandler with pressure control

--- gdrive-api/src/uploadHandler.js
+++ gdrive-api/src/uploadHandler.js
@@ -4,22 +4,48 @@ import { pipeline } from 'stream/promises';
 import { logger } from './logger';
 
 export default class UploadHandler {
-  constructor({ io, socketId, downloadsFolder }) {
+  constructor({ io, socketId, downloadsFolder, messageTimeDelay = 200 }) {
     this.io = io;
     this.socketId = socketId;
     this.downloadsFolder = downloadsFolder;
+    this.ON_UPLOAD_EVENT = 'file-upload';
+    this.messageTimeDelay = messageTimeDelay
   }
 
-  handleFileBytes() {}
+  canExecute(lastExecution) {
+    return (Date.now() - lastExecution) >= this.messageTimeDelay;
+  }
+
+  handleFileBytes(filename) {
+    this.lastMessageSent = Date.now();
+
+    async function* handleData(source) {
+      let processedAlready = 0;
+      
+      for await (const chunk of source) {
+        yield chunk;
+
+        processedAlready += chunk.length;
+        if(!this.canExecute(this.lastMessageSent)) {
+          continue;
+        }
+        this.lastMessageSent = Date.now();
+        this.io
+          .to(this.socketId)
+          .emit(this.ON_UPLOAD_EVENT, { processedAlready, filename });
+        logger.info(
+          `File [${filename}] got ${processedAlready} bytes to ${this.socketId}`
+        );
+      }
+    }
+    return handleData.bind(this);
+  }
 
   async onFile(fieldname, file, filename) {
     const saveTo = `${this.downloadsFolder}/${filename}`;
     await pipeline(
-      // 1 passo, pegar uma readable stream
       file,
-      // 2 passo, filtrar, converter, transformar dados
       this.handleFileBytes.apply(this, [filename]),
-      // 3 passo, é a saída do processo, uma writable stream
       fs.createWriteStream(saveTo)
     );
 
--- gdrive-api/test/_util/testUtil.js
+++ gdrive-api/test/_util/testUtil.js
@@ -1,9 +1,22 @@
-import { read } from 'fs';
+import { jest } from '@jest/globals';
 import { Readable, Writable, Transform } from 'stream';
 
 export default class TestUtil {
+  static mockDateNow(mockImplementationPeriods) {
+    const now = jest.spyOn(global.Date, global.Date.now.name);
+
+    mockImplementationPeriods.forEach((time) => {
+      now.mockReturnValueOnce(time);
+    });
+  }
+
+  static getTimeFromDate(dateString) {
+    return new Date(dateString).getTime();
+  }
+
   static generateReadableStream(data) {
     return new Readable({
+      objectMode: true,
       read() {
         for (const item of data) {
           this.push(item);
@@ -26,12 +39,18 @@ export default class TestUtil {
   }
 
   static generateTransformStream(onTransform) {
+    // async function* handle(source) {
+    //   for await(const chunk of source) {
+    //     yield chunk
+    //   }
+    // }
+
     return new Transform({
       objectMode: true,
       transform(chunk, encoding, cb) {
         onTransform(chunk);
         cb(null, chunk);
-      }
-    })
+      },
+    });
   }
 }
